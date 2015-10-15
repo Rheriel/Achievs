0e4fb9b9da76559f4fe968303fbdc5280ab0f688
Rheriel <rheriel@hotmail.co.jp>
2015-10-15 00:13:55 -0500

    Installed Angular
    Installed Angular using Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name achievsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the achievsApp
+ */
+angular.module('achievsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name achievsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the achievsApp
+ */
+angular.module('achievsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
